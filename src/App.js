1611a42b3f0dcf0d896dbf8f3dd6f5e1ec45e96a
Gustavo Raña <gustavorana25@gmail.com>
2018-09-28 06:32:11 -0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/home';
+import Movie from './pages/movie';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/filme" component={Movie} />
+          <Route path="*" component={Home} />
+        </Switch>
+      </ BrowserRouter>
     );
   }
 }
---
+++ src/pages/movie.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MoviePage extends Component {
+    render() {
+        return (
+            <main>
+                movie
+            </main>
+        )
+    }
+}
+
+export default MoviePage;
